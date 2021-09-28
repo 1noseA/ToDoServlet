0cc6e800c9c5850b25ffc27c1e944a0b39a67d06
1noseA <1noseA2L@gmail.com>
2021-09-28 23:15:13 +0900

    削除機能追加

--- src/com/sample/ToDoDeleteServlet.java
+++ src/com/sample/ToDoDeleteServlet.java
@@ -1,17 +1,20 @@
 package com.sample;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class ToDoDeleteServlet
  */
-@WebServlet("/Delete")
+@WebServlet("/delete")
 public class ToDoDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -27,8 +30,23 @@ public class ToDoDeleteServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		List<ToDo> list = (List<ToDo>) session.getAttribute("list");
+
+		int id = Integer.parseInt(request.getParameter("id"));
+
+		for (ToDo todo : list) {
+			if (todo.getId() == id) {
+				list.remove(todo);
+				break;
+			}
+		}
+
+		session.setAttribute("list", list);
+
+		RequestDispatcher rd = request.getRequestDispatcher("/todo.jsp");
+		rd.forward(request, response);
+		return;
 	}
 
 	/**
